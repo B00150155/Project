dc9c08faf42eaf02c6b89c0ca463da712f813dfd
Zaman Zaidi <146003935+B00150155@users.noreply.github.com>
2023-11-15 17:18:04 +0000

    Base Game/No mechanics

---
+++ ShootTheTarget/Assets/Scripts/FPSController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{   
+    public Camera playerCamera;
+
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    public bool canMove = true;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if(canMove){
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
